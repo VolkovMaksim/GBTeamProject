973a46577d811ce31a10f6f58ce82bcfd7492123
Maksim Volkov <volkovmd.dev@mail.ru>
2023-02-06 07:09:23 +0300

    ID-028 Create upload merchlist

---
+++ Sources/App/Models/Merch/Merch.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maksim Volkov on 06.02.2023.
+//
+
+import Foundation
+
+struct MerchElement: Codable {
+    let name: String
+    let price, size: Int
+    let color, icon: String
+    let feedbacks: [String]
+}
+
+typealias Merch = [String: [MerchElement]]
